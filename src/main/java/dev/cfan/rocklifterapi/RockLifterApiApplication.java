5614c9c7b709af40c6b148b7335e4959d1caeb46
Christopher Fanning <chris@christopherfanning.com>
2022-02-23 10:15:31 -0600

    Initial commit.

---
+++ src/main/java/dev/cfan/rocklifterapi/RockLifterApiApplication.java
@@ -0,0 +1,13 @@
+package dev.cfan.rocklifterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockLifterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockLifterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cfan/rocklifterapi/RockLifterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cfan.rocklifterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockLifterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
