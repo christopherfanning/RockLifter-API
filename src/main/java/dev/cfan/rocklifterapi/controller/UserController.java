7b6b89fe9b8f21ec920278605b36f1b28c032a56
Christopher Fanning <chris@christopherfanning.com>
2022-02-25 13:30:55 -0600

    Add data to users entity.

--- src/main/java/dev/cfan/rocklifterapi/controller/UserController.java
+++ src/main/java/dev/cfan/rocklifterapi/controller/UserController.java
@@ -1,4 +1,14 @@
 package dev.cfan.rocklifterapi.controller;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class UserController {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
