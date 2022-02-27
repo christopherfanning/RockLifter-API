b77450fd4a16f98dc70038eba6349c274cd1170b
Christopher Fanning <chris@christopherfanning.com>
2022-02-27 15:24:32 -0600

    Add model for Role and add Roles to User

--- src/main/java/dev/cfan/rocklifterapi/model/Role.java
+++ src/main/java/dev/cfan/rocklifterapi/model/Role.java
@@ -1,30 +1,25 @@
 package dev.cfan.rocklifterapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
+
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-public class Issue {
-
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-    private String title;
-    private String description;
-    private String status;
+    private String role;
 
     @JsonIgnore
-    @ManyToOne
-    @JoinColumn(name = "project_id")
-    private Project project;
-
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    User user;
 }
--- src/main/java/dev/cfan/rocklifterapi/model/User.java
+++ src/main/java/dev/cfan/rocklifterapi/model/User.java
@@ -1,29 +1,56 @@
 package dev.cfan.rocklifterapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
+    @Column
     private String name;
+
+    // toLowerCase setter
+    public void setName(String name) {
+        name = name.toLowerCase();
+        this.name = name;
+    }
+
+    @Column
+    private String email;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-    private String userRole;
 
 
+    // Might need this for Integration tests.
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @OneToMany(mappedBy = "user")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Issue> issueList;
+
+
+    @OneToOne(mappedBy = "user", orphanRemoval = true)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Role userRole;
 
 }
